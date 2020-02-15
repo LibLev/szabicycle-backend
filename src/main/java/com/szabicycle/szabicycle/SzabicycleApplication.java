62b632af1d7a13b863a63ba2f64fbd8a5a7acbb8
LibLev <libardi.levente@gmail.com>
2020-02-15 11:09:40 +0100

    Initial commit

---
+++ src/main/java/com/szabicycle/szabicycle/SzabicycleApplication.java
@@ -0,0 +1,13 @@
+package com.szabicycle.szabicycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzabicycleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzabicycleApplication.class, args);
+    }
+
+}
