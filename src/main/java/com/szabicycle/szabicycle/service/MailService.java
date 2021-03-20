9b9d0bc5c5098289b5b411f120b0b9a587906ad0
LibLev <libardi.levente@gmail.com>
2021-03-20 19:10:31 +0100

    sending feedback mail to the customer

--- src/main/java/com/szabicycle/szabicycle/service/MailService.java
+++ src/main/java/com/szabicycle/szabicycle/service/MailService.java
@@ -18,12 +18,20 @@ public class MailService {
     }
 
     public void sendMail(Map<String, String> data){
-        log.info(data.toString());
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setTo("szabicycle@gmail.com");
         mail.setFrom(data.get("emailAddress"));
         mail.setSubject(data.get("subject"));
         mail.setText(data.get("emailAddress")+"\n"+data.get("message"));
         javaMailSender.send(mail);
+        SimpleMailMessage mail2 = new SimpleMailMessage();
+        mail2.setTo(data.get("emailAddress"));
+        mail2.setFrom(data.get("emailAddress"));
+        mail2.setSubject("SZABICYCELNEK A KÖVET KEZŐ LEVELET KÜLDTED :"+data.get("subject"));
+        mail2.setText("\""+data.get("message")+"\""+"\n\nHamarosan felvesszük veled a kapcsolatot." +
+                "\n Addig is türelmedet kérjük!" +
+                "\n\nÜdvözlettel" +
+                "\nSzabiCycle");
+        javaMailSender.send(mail2);
     }
 }
